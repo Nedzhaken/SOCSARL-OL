a1891e0309b6bb37a2870aab7e0bcebe83ab038c
Nezhaken <okun.29@mail.ru>
2026-03-02 12:02:01 +0100

    Cansel test

--- Magni/src/trajectory.py
+++ Magni/src/trajectory.py
@@ -13,8 +13,7 @@ class Trajectory:
         """
         Return the list of X coordinates of the trajectory.
         """
-        return 0
-        # return self.x
+        return self.x
     
     def get_traject_y(self):
         """
